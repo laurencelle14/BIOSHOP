38d12efafe5debcbb76faf1b6645aa0d4ba10c5e
laurencelle14 <laurencelleakpa18@gmail.com>
2026-06-08 16:35:50 +0000

    retrait de backend dans la branche du frontend

--- backend/apps/reviews/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Review
-
-
-class ReviewSerializer(serializers.ModelSerializer):
-    class Meta :
-        model = Review
-        fields = ['user','product', 'rating', 'comment', 'created_at']
--- backend/apps/reviews/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from .views import ReviewViewset
-
-router = DefaultRouter()
-router.register(r'reviews', ReviewViewset, basename='review')
-
-
-urlpatterns = router.urls
