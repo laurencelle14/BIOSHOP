fcfc6a936ff04ea70ef623d1ed6a9af1f81cb645
laurencelle14 <laurencelleakpa18@gmail.com>
2026-06-19 21:41:48 +0000

    fix: retour à l'initial

--- backend/bioshop/settings.py
+++ backend/bioshop/settings.py
@@ -140,16 +140,12 @@ AUTH_USER_MODEL = 'users.CustomUser'
 
 # Email
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-
-EMAIL_HOST = 'smtp.resend.com'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-
-EMAIL_HOST_USER = 'resend'
-EMAIL_HOST_PASSWORD = os.getenv('RESEND_API_KEY')
-
-DEFAULT_FROM_EMAIL = 'onboarding@resend.dev'
-EMAIL_TIMEOUT = 10
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 # Sécurité en prod
 if not DEBUG:
